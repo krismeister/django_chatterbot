bc78e8abb460387fcb402c47d2e7023ea24362c0
Roberto Andrade <roberto@cloud.com>
2016-02-22 17:39:58 -0500

    Fixing issues running the app.

--- example/django_chatterbot/api.py
+++ example/django_chatterbot/api.py
@@ -8,6 +8,16 @@ chatterbot = ChatBot(
     io_adapter="chatterbot.adapters.io.JsonAdapter"
 )
 
+# Train based on the english corpus
+chatterbot.train("chatterbot.corpus.english")
+
+# Train based on english greetings corpus
+chatterbot.train("chatterbot.corpus.english.greetings")
+
+# Train based on the english conversations corpus
+chatterbot.train("chatterbot.corpus.english.conversations")
+
+# Custom training
 chatterbot.train([
     "Hi",
     "Hello",
@@ -32,7 +42,8 @@ class ChatterBotView(View):
     def post(self, request, *args, **kwargs):
         input_statement = request.POST.get('text')
 
-        response_data = chatterbot.get_response(input_statement)
+        response_data = {
+            'text': chatterbot.get_response(input_statement)
+        }
 
         return JsonResponse(response_data)
-
